23d094200986d378599612b014131756b3b3b1fb
skills-surendra <40684316+skills-surendra@users.noreply.github.com>
2018-07-03 15:19:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+	$method = $_SERVER['REQUEST_METHOD'];
+	if($method == "POST") {
+		$response = array();
+
+		$requestbody = file_get_contents('php://input');
+
+		$json_data = json_decode($requestbody); 
+
+		$responseId= $json_data->responseId;
+		$queryResult= $json_data->queryResult;
+
+		$answer= strtolower($queryResult->queryText);
+		$action=$queryResult->action;
+
+		$parameters = $queryResult->parameters;
+		$intent= $queryResult->intent;
+		$intentNameArr=explode('/',$intent->name);
+
+		$project_id=$intentNameArr[1];
+		$session_id=$intentNameArr[4];
+
+
+
+		require_once 'include/common.php';
+		
+
+
+		if($action=='welcome'){
+			$response=welcome();
+		}
+		else if($action=='mobile_verification'){
+			$response=mobile_verification();
+		}
+		else if($action=='input.unknown'){
+			$response=not_matched();
+		}
+		
+		elseif ($action=='check-product-item-available') {
+			$response=check_product_item_available();
+		}
+		elseif ($action=='CheckItemAvailable.CheckItemAvailable-order') {
+			$response=check_item_available_order();
+		}
+
+		
+		//print_r($response); die;
+
+
+		$response['source'] = "webhook";
+		echo json_encode($response); die;
+
+	}
+ ?>
